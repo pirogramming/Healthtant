89647fe4312691d3b610f90fa3bb26d85e6a0914
Github GGo-Dong <kimdongsu481@gmail.com>
2025-08-06 20:36:46 +0900

    feat: analysis 앱 urls.py 생성

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('accounts.urls')),
     path('diets/', include('diets.urls')),
+    path('analysis/', include('analysis.urls')),
 ]
