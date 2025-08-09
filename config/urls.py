e9a18449af8a5a0776db434b2936f52ceda4fe73
mnjese <mjk5949@naver.com>
2025-08-09 15:25:23 +0900

    feat: 제품 상세 페이지 및 즐겨찾기 기능 구현

--- config/urls.py
+++ config/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('diets/', include('diets.urls')),
     path('analysis/', include('analysis.urls')),
+    path('products/', include('products.urls')),
 ]
