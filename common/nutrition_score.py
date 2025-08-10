8ac8d7a9bc938b645b076354e3d6035a6d33f735
KimDongSu <kimdongsu481@gmail.com>
2025-08-11 01:34:45 +0900

    feat: 임시 NutritionalScore (영양 점수 계산 메소드) 구현

--- common/nutrition_score.py
+++ common/nutrition_score.py
@@ -1,9 +1,27 @@
 from foods.models import Food
 
+#---------------------------------------여기부터 내부적으로만 사용하는 메소드! 다른 앱에서 직접 호출할 일은 없음!--------------------------
 # 나트륨(mg)을 받아 소금(g)으로 변환하는 함수
 def sodium_into_salt(sodium):
     return sodium*2.5/1000
 
+#100kcal에 함유된 nutrient의 양을 반환하는 함수
+def get_100kcal_nutrient(food, nutrient):
+    calorie = getattr(food, "calorie")
+    #제로 음식인 경우
+    if calorie == 0 or calorie == None:
+        return 0
+    
+    nutirent_100kcal = getattr(food, nutrient, 0)/calorie * 100
+    return nutirent_100kcal
+
+def NRFgoodNutrient(nutrient, require):
+    return min(100, nutrient/require*100)
+
+def NRFbadNutrient(nutrient, require):
+    return nutrient/require * 100
+
+#----------------------------------------여기부터 직접 사용하면 되는 메소드!-------------------------------------------------------------
 #영양소 이름과 food를 입력하면 영양소 함량이 GOOD, NEUTRAL, BAD 인지, 그리고 낮음/적정/높음 인지 반환하는 함수
 # *주의* 영양적인 관점에서 칼로리는 낮고 높은 기준을 명시하지 않는게 좋음!! 칼로리가 낮다고 좋은 음식인게 아니고 칼로리가 높다고 안 좋은 음식인게 아니기 때문!
 # 따라서 칼로리에 대한 GOOD, BAD 문구는 출력하지 않는걸로 합시다!
@@ -38,10 +56,45 @@ def get_level(nutrient, food):
     #단백질은 음료와 고형식 기준이 동일함
     if nutrient == "protein":
         protein = getattr(food, "protein", 0)
-        calorie = getattr(food, "calorie") #칼로리는 0이 아님이 보장됨
+        calorie = getattr(food, "calorie", 1)
         ratio = protein*4/getattr(food, "calorie") * 100
         if ratio < 12: return {"level": "낮음", "class": "BAD"}
         elif ratio < 20: return {"level": "적정", "class": "NEUTRAL"}
         else: return {"level": "높음", "class": "GOOD"}
 
     return {"level": "판정불가", "class": None}
+
+
+# food의 NRF 지수를 계산하는 함수
+def NRF(food):
+    protein_score = NRFgoodNutrient(get_100kcal_nutrient(food, "protein"), 55)
+    dieatry_fiber_score = NRFgoodNutrient(get_100kcal_nutrient(food, "dietary_fiber"), 25)
+    vitaminA_score = NRFgoodNutrient(get_100kcal_nutrient(food, "VitaminA"), 700)
+    vitaminC_score = NRFgoodNutrient(get_100kcal_nutrient(food, "VitaminC"), 100)
+    vitaminE_score = NRFgoodNutrient(get_100kcal_nutrient(food, "VitaminE"), 11)
+    calcium_score = NRFgoodNutrient(get_100kcal_nutrient(food, "calcium"), 700)
+    iron_content_score = NRFgoodNutrient(get_100kcal_nutrient(food, "iron_content"), 12)
+    potassium_score = NRFgoodNutrient(get_100kcal_nutrient(food, "potassium"), 3500)
+    magnesium_score = NRFgoodNutrient(get_100kcal_nutrient(food, "magnesium"), 315)
+
+    saturated_fatty_acids_score = NRFbadNutrient(get_100kcal_nutrient(food, "saturated_fatty_acids"), 15)
+    sugar_score = NRFbadNutrient(get_100kcal_nutrient(food, "sugar"), 100)
+    salt_score = NRFbadNutrient(get_100kcal_nutrient(food, "salt"), 2000)
+
+    good_score_sum = (protein_score + dieatry_fiber_score + vitaminA_score + vitaminC_score + vitaminE_score + calcium_score + potassium_score + iron_content_score + magnesium_score)/9
+    bad_score_sum = (saturated_fatty_acids_score + sugar_score + salt_score)/3
+
+    return good_score_sum - bad_score_sum
+
+def NutritionalScore(food):
+    NRFscore = NRF(food)
+    if NRFscore >= 60:
+        return "A"
+    elif NRFscore >= 40:
+        return "B"
+    elif NRFscore >= 20:
+        return "C"
+    elif NRFscore >= 0:
+        return "D"
+    else:
+        return "E"
