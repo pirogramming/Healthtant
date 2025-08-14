b67b6fb091cd4db51f361923733487316871b6c3
mnjese <mjk5949@naver.com>
2025-08-15 05:35:55 +0900

    feat: add search app and update local settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,7 +3,6 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -18,8 +17,8 @@ DATABASES = {
         'NAME': str(BASE_DIR / 'db.sqlite3'),
     },
 }
-'''
 
+'''
 # SQLite 사용 (개발용)
 DATABASES = {
     'default': {
@@ -27,3 +26,4 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
