b68877e41ec01c68920c049fd998133bb0d24934
KimDongSu <kimdongsu481@gmail.com>
2025-08-16 03:41:19 +0900

    feat: 하단 nav바 고급검색 페이지와 연결

--- search/urls.py
+++ search/urls.py
@@ -6,4 +6,5 @@ app_name = 'search'
 urlpatterns = [
     path('', views.search_page, name='search_page'),
     path('normal/', views.normal_search, name='normal_search'),
+    path('advanced/page/', views.advanced_search_page, name='diet_search'),
 ]
