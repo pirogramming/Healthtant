63690e636bde93211fde76a0fb65f4156b87fcd6
mnjese <mjk5949@naver.com>
2025-08-19 18:15:05 +0900

    fix: 디버깅 추가

--- config/settings/production.py
+++ config/settings/production.py
@@ -16,5 +16,33 @@ DATABASES = {
     }
 }
 
+# 로깅 설정 (임시로 추가 - 디버깅용)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/app/django_errors.log',
+        },
+        'console': {
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['file', 'console'],
+        'level': 'ERROR',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file', 'console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
+
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
